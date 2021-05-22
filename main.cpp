4a8a31872e1470010ed0f2dd8b628a63fed15341
ibrahim317 <74654255+ibrahim317@users.noreply.github.com>
2021-05-22 12:26:11 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ifstream infile;
+    ofstream outfile;
+    string spath;
+    string tpath;
+    cout << "inter the source file path :";
+    cin >> spath;
+    infile.open(spath);
+    if (infile.fail())
+    {
+        cout << "can`t find the file in the path that you intered please cheak the name";
+        exit(1);
+    }
+    cout << "inter the target file path :";
+    cin >> tpath;
+    outfile.open(tpath);
+
+    string contant;
+    for (int i = 0; infile.eof() != true; i++)
+    {
+        contant += infile.get();
+    }
+    outfile << contant;
+    return 0;
+}
